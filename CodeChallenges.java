8934f9e05c44fa555e17e6589cd2fe7cfd5b9fec
Pfulcher26 <pfulcher26@gamil.com>
2023-06-23 10:32:22 -0700

    Adds solution for countingSort.

--- CodeChallenges.java
+++ CodeChallenges.java
@@ -15,14 +15,15 @@ public class CodeChallenges {
         diagonalTwo += arr.get(index).get(negIndex);
         index ++;
         negIndex --; 
-
     }
     
     return Math.abs(diagonalOne - diagonalTwo); 
   }
   
   public static int LonelyInteger(List<Integer> arr){
+
     HashSet<Integer> set = new HashSet<>();
+    
     for(Integer num: arr){
       if(set.contains(num)){
         set.remove(num);
@@ -30,8 +31,26 @@ public class CodeChallenges {
         set.add(num);
       }
     }
+
     return set.iterator().next();
   }
+
+  public static int[] countingSort(int[] arr){
+    int[] frequencyArr = new int[100];
+    for(int i = 0; i < arr.length; i++){
+      int numValue = arr[i];
+      frequencyArr[numValue] ++;
+    }
+    return frequencyArr; 
+  }
+
+  public static int[] createArrayFrom1To100() {
+    int[] arr = new int[100];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = i;
+    }
+    return arr;
+}
 
   public static void main(String[] args) {
     List<List<Integer>> arr = new ArrayList<>();
@@ -40,6 +59,14 @@ public class CodeChallenges {
     arr.add(List.of(9, 8, 9));
 
     int result = diagonalDifference(arr);
+
+    int[] testArr = createArrayFrom1To100();
+    int[] countSortedArray = countingSort(testArr);
+
+    for(int i = 0; i < countSortedArray.length; i++){
+      System.out.println("current value is " + testArr[i]);
+    }
+
     System.out.println(result);
   }
 }
