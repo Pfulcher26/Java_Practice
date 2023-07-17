eb1bc9623d55d1edadd262b4a32f9b4a54a07ef4
Pfulcher26 <pfulcher26@gamil.com>
2023-07-17 11:07:40 -0700

    Uses a sliding window technique to solve Leetcode challenge Max Consecutive Ones III

--- CodeChallenges.java
+++ CodeChallenges.java
@@ -211,6 +211,29 @@ public class CodeChallenges {
     return maxAverage;        
     }
 
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int current = 0;
+        int answer = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                current++;
+            }
+
+            while (current > k) {
+                if (nums[left] == 0) {
+                    current--;
+                }
+                left++;
+            }
+
+            // Update the answer inside the loop to consider all possible windows
+            answer = Math.max(answer, right - left + 1);
+        }
+
+        return answer;
+    }
 
   public static void main(String[] args) {
     List<List<Integer>> arr = new ArrayList<>();
