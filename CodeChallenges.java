457178f1475b88d4913a8cf367f54f3699b644de
Pfulcher26 <pfulcher26@gamil.com>
2023-07-23 19:33:15 -0700

    Adds solution for maxVowels using a sliding window technique.

--- CodeChallenges.java
+++ CodeChallenges.java
@@ -5,6 +5,40 @@ import java.util.List;
 
 public class CodeChallenges {
 
+  public int maxVowels(String s, int k) {
+        
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        int maxVowels = 0;
+        int currentVowels = 0;
+
+        for(int i = 0; i < k; i++){
+            if(vowels.contains(s.charAt(i))) {
+                currentVowels++; 
+            }
+        }
+
+        maxVowels = Math.max(maxVowels, currentVowels);
+
+        for(int i = k; i < s.length(); i++){
+            if(vowels.contains(s.charAt(i))) {
+                currentVowels++;
+            }
+            if(vowels.contains(s.charAt(i - k))) {
+                currentVowels--; 
+            }
+            maxVowels = Math.max(maxVowels, currentVowels);
+        }
+
+       return maxVowels; 
+
+    }
+
   public static List<Integer> breakingRecords(List<Integer> scores) {
         int minScore = scores.get(0);
         int maxScore = scores.get(0);
